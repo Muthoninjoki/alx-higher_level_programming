bbe6c665d5d7964274f561751be8ba546bce7b34
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-08-24 16:29:20 +0000

    Prints all possible different combinations of 2 digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    if n != 99:
-        print("{:02}, ".format(n), end="")
+    if n == 99:
+        print(n)
     else:
-        print("99")
+        print("{:0>2d}".format(n), end=", ")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for n in range(0, 9):
+    for i in range(n + 1, 10):
+        if n == 8 and i == 9:
+            print("{:d}{}".format(i, j))
+        print("{}{}".format(n, i), end=", ")
