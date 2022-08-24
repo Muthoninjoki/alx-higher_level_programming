7872746b6a25f8c10a26fc785c3e5ad5a23db22d
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-08-24 20:07:18 +0000

    Adds 2 int and returns result

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randit(-10, 10)
 if number > 0:
     print("{:d} is positive".format(number))
-elif number == 0:
-    print("{:d} is zero".format(number))
-else:
+elif number < 0:
     print("{:d} is negative".format(number))
+else:
+    print("{:d} is zero".format(number))
---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    ans = a + b
+    return(ans)
