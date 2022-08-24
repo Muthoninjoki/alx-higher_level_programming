8e7a4b15bc56372ee3d8df11be15e7f969b67998
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-08-24 20:23:19 +0000

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 15 == 0:
+            print("{} ".format("FizzBuzz"), end='')
+        elif n % 5 == 0:
+            print("{} ".format("Buzz"), end='')
+        elif n % 3 == 0:
+            print("{} ".format("Fizz"), end='')
+        else:
+            print("{} ".format(i), end='')
