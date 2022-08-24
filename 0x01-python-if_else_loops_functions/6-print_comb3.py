f146d1ca7d22cb72ce99a7fc99add37b360b97bd
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-08-24 16:35:42 +0000

    Checks for lowercase character

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for n in range(0, 9):
     for i in range(n + 1, 10):
         if n == 8 and i == 9:
-            print("{:d}{}".format(i, j))
+            print("{:d}{}".format(n, i))
         print("{}{}".format(n, i), end=", ")
