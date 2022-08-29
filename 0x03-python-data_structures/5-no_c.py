c438732339045e8406c41f84110ad08238b55ce8
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-08-29 14:45:23 +0000

    Prints a matrix of integers

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for l in range(len(my_string)):
-        if my_string[l] == "c" or my_string[l] == "C":
+    for letter in range(len(my_string)):
+        if my_string[letter] == "c" or my_string[letter] == "C":
             pass
         else:
-            new_string += my_string[l]
+            new_string += my_string[letter]
     return new_string
