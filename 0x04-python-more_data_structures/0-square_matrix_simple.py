88dd1bc739678ad7553697f051fca31267f257ab
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-02 19:44:03 +0000

    Computes the square valu of all ints in a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    def power_2(x):
+        return x ** 2
+
+    squares = []
+    for n in matrix:
+        squares.append(list(map(power_2, i)))
+
+    return squares
