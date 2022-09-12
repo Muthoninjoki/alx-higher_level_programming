c0c26e21bad10b1addc84084950df56c043cf737
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-12 13:27:37 +0000

    Function that divides 2 ints and prints the result

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            counter += 1
-        except:
-            pass
+    i = 0
+    try:
+        while(x > 0):
+            print("{}".format(my_list[i]), end="")
+            x -= 1
+            i += 1
+    except IndexError:
+        pass
     print()
-    return counter
+    return i
