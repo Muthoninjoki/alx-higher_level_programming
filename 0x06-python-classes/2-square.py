0351ab8fde0c32eb3363c199ece0eee9706ad9e1
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-14 15:54:31 +0000

    Defines a square by:(based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square():
 
     def __init__(self, size=0):
         if (type(size) is not int):
-            raise TypeError("size should be an int")
+            raise TypeError("size must be an integer")
         elif (size < 0):
-            raise ValueError("size should be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
