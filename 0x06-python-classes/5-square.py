5753e5b84270bbcf8c6af5bd3edf7b400154b548
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-14 18:41:49 +0000

    Defines a square based on 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,29 +2,30 @@
 """class Square that defines a square"""
 
 
-class Square():
-    """square class with its proper validation and size"""
-
-    def __init__(self, size=0):
-        self.__size = size
+class Square:
+    def __init(self, size=0):
+        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif (value < 0):
-            raise ValueError("size must be >= 0"):
-        self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            slef.__size = value
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size ** 2)
 
     def my_print(self):
-        if not self.__size:
+        """print square to stdout in # char"""
+        for x in range(0, self.__size):
+            [print("#", end="") for y in range(self.__size)]
             print("")
-        for i in range(self.__size):
-            print("#" * self.__size)
+        if self.__size == 0:
+            print("")
