1c2e881124d79d1da35de741f617c49551dc9ba1
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-14 15:43:09 +0000

    Defines a square by:(based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Working on op with python"""
+
+
+class Square():
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """ initialize the class with a private square attribute"""
+        self.__size = size
