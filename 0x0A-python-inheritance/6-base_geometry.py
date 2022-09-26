09ad2d9e46bb91752f6bca49397b0024e906eda6
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-26 17:01:28 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """No exception implemented"""
+        raise Exception("area() is not implemented")
