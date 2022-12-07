44fd8ffdb5ef1df232dbbfe8ba0f31ea9b12fc55
Muthoni <fmuthoninjoki@gmail.com>
2022-12-07 10:36:01 -0800

    Cities by states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- lists all states from the database hbtn_0e_0_usa
+takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
 """
 
 import MySQLdb
@@ -11,10 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
