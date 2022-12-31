f57973ca8bc124296378932105a9eae297bf3052
Muthoni <fmuthoninjoki@gmail.com>
2022-12-31 06:51:41 -0800

    cURL a JSON file

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that sends a request to a URL passed as an
-#argument, and displays only the status code of the response.
+# sends a request to a url, displays only status code 
 curl -s -o /dev/null -w "%{http_code}" "$1"
---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that sends a JSON POST request to a URL
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
