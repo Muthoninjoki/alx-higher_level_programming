037b3250975cc9b40de44d6e3f55a1c535ddc2a7
Muthoni <fmuthoninjoki@gmail.com>
2023-04-28 08:36:46 -0700

    Add python script to fetch and display response body using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status
+This script fetches https://alx-intranet.hbtn.io/status using urllib package and displays the body of the response.
 """
+
 import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html.decode("utf-8")))
