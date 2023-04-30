d886801ab93cdacc1d66f97fbe5e3c72dd5d6103
Muthoni <fmuthoninjoki@gmail.com>
2023-04-30 03:10:04 -0700

    Python script to fetch X-Request-Id variable from URL header using requests package

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the value of the variable"""
+"""
+Takes in a URL, sends a request to the URL and displays the value of the variable
+X-Request-Id in the response header
+"""
+
 import request
 import sys
 
